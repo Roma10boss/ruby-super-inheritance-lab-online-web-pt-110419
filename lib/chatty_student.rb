ed551b07d15cd7c98afa19034c533287552adcd5
Romario Gustave <romagustave10@gmail.com>
2020-01-08 00:26:34 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,10 +1,10 @@
 class ChattyStudent < Student 
   def hello 
     super
-    puts "How are you doing today"
+    puts "How are you doing today? I'm okay, but I'm kind of tired. did you watch the walking dead last night? you didn't?oh man, it was so crazy1 What you don't want any spoilers? Okay well let me tell you who died ..."
   end 
   
   def raise_hand 
-    10.times(super)
+    10.times{super}
   end 
 end 
