90a33a82eeb770b245eb300376ab999e40440338
Romario Gustave <romagustave10@gmail.com>
2020-01-08 00:41:53 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,7 @@
 class ChattyStudent < Student 
   def hello 
     super
-    puts "Hey there! I'm so excited to learn stuff.\nHey there! I'm so excited to learn stuff. How are you doing...n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    puts "Hey there! I'm so excited to learn stuff.\nHey there! I'm so excited to learn stuff. How are you doing...\n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end 
   
   def raise_hand 
